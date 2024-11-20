3ac2cdea8b123f8b2d5944a2d51d90ea292019ea
sigmotoa <si.galvismotoa@gmail.com>
2024-11-20 10:37:36 -0600

    Version3

---
+++ model.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    descripcion:str
+    status:str
+
+class Todo(BaseModel):
+    id:int
+    item:Item
--- todo.py
+++ todo.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
+from model import Todo
 
-todo_list = []
+todo_list:Todo = []
 
 todo = APIRouter()
 
 @todo.post("/todo")
-def add_todo(todo:dict):
+def add_todo(todo:Todo):
     todo_list.append(todo)
     return {"message":"Added"}
 
