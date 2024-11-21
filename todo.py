a4373beea1cd3c40fd92be0e90fcc5d688428001
sigmotoa <si.galvismotoa@gmail.com>
2024-11-21 15:05:30 -0600

    Version 5

--- todo.py
+++ todo.py
@@ -22,3 +22,12 @@ def show_one_todo(todo_id:int=Path(...,title="ID")):
         if todo.id == todo_id:
             return {"todo":todo}
     return {"message":"ID doesn't exist"}
+
+@todo.delete("/todo/{todo_id}")
+def delete_one(todo_id:int=Path(...)):
+    for i in range(len(todo_list)):
+        todo = todo_list[i]
+        if todo.id == todo_id:
+            todo_list.pop(i)
+            return {"message":"Deleted done"}
+    return {"message":"Id doesn't exist"}
